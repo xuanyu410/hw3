02f70561d0432b77d5cc2da08979e8eeadb7a699
xuanyu410 <jessicayang950410@gmail.com>
2025-10-21 10:23:09 +0800

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import AItest from './AItest';
-
-const el = document.getElementById('react-root');
-if (el) {
-  createRoot(el).render(<AItest />);
-}
